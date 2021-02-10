942e61ca7fcee31b62cede756e3e96dfc892c759
hongstudio <hsd1525@naver.com>
2021-02-10 16:51:27 +0900

    ProductActivity 레이아웃 구성 완료

--- app/src/main/java/com/hong_studio/myauctionapp/ProductActivity.java
+++ app/src/main/java/com/hong_studio/myauctionapp/ProductActivity.java
@@ -1,26 +1,35 @@
 package com.hong_studio.myauctionapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.tbuonomo.viewpagerdotsindicator.DotsIndicator;
 
 import java.util.ArrayList;
 
 public class ProductActivity extends AppCompatActivity {
 
+    ArrayList<Integer> imgIds= new ArrayList<>();
     DotsIndicator dotsIndicator;
     ViewPager viewPager;
     ImageViewPagerAdapter adapter;
-
-    ArrayList<Integer> imgIds= new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+
+        MaterialToolbar toolbar= findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         loadData();
 
@@ -29,6 +38,22 @@ public class ProductActivity extends AppCompatActivity {
         adapter = new ImageViewPagerAdapter(this, imgIds);
         viewPager.setAdapter(adapter);
         dotsIndicator.setViewPager(viewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu2, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     void loadData(){
