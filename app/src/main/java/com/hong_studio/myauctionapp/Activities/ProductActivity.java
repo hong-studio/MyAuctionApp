bc028494c6401cec5caf155318aa0637626c26d1
hongstudio <hsd1525@naver.com>
2021-02-22 23:04:48 +0900

    ViewPager있는 버전으로 일단 dev3에 저장(나중에 시도해보기)

--- app/src/main/java/com/hong_studio/myauctionapp/Activities/ProductActivity.java
+++ app/src/main/java/com/hong_studio/myauctionapp/Activities/ProductActivity.java
@@ -46,6 +46,8 @@ public class ProductActivity extends AppCompatActivity {
         setViewPagerAndDotsIndicator();
         setLayoutProfile();
 
+        //findViewByID...
+
         onClickHeart();
     }
 
