a72c54c1c5cddac75c3902958b28b6d5b33165ae
hongstudio <hsd1525@naver.com>
2021-02-28 00:19:37 +0900

    애니메이션 줄 준비완료

--- app/src/main/java/com/hong_studio/myauctionapp/Tab4/CustomerCenterActivity.java
+++ app/src/main/java/com/hong_studio/myauctionapp/Tab4/CustomerCenterActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,7 +17,7 @@ public class CustomerCenterActivity extends AppCompatActivity {
 
     TextView tv01, tv02;
     ImageView iv01Down, iv02Down;
-    TextView tv01Down, tv02Down;
+    TextView tv01whenDown, tv02whenDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +30,36 @@ public class CustomerCenterActivity extends AppCompatActivity {
         tv02= findViewById(R.id.tv02);
         iv01Down= findViewById(R.id.iv01_down);
         iv02Down= findViewById(R.id.iv02_down);
-        tv01Down= findViewById(R.id.tv01_down);
-        tv02Down= findViewById(R.id.tv02_down);
+        tv01whenDown= findViewById(R.id.tv01_whenDown);
+        tv02whenDown= findViewById(R.id.tv02_whenDown);
+
+        iv01Down.setOnClickListener(new View.OnClickListener() {
+            int isClicked= 0;
+            @Override
+            public void onClick(View v) {
+                if(isClicked==0){
+                    tv01whenDown.setVisibility(View.VISIBLE);
+                    isClicked= 1;
+                } else if(isClicked!=0){
+                    tv01whenDown.setVisibility(View.GONE);
+                    isClicked= 0;
+                }
+            }
+        });
+
+        iv02Down.setOnClickListener(new View.OnClickListener() {
+            int isClicked= 0;
+            @Override
+            public void onClick(View v) {
+                if(isClicked==0){
+                    tv02whenDown.setVisibility(View.VISIBLE);
+                    isClicked= 1;
+                } else if(isClicked!=0){
+                    tv02whenDown.setVisibility(View.GONE);
+                    isClicked= 0;
+                }
+            }
+        });
     }
 
     @Override
